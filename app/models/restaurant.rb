c36811f7f5bc94798a1f153b7e4f464edcf92688
Abe Kor <ihkor@mun.ca>
2025-06-11 19:25:46 -0230

    generate map

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,9 @@ class Restaurant < ApplicationRecord
   has_many :deals, dependent: :destroy
   validates :name, presence: true
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   # Order by name by default
   default_scope { order(name: :asc) }
 end
