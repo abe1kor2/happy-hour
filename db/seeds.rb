f9232f16fba8792579508834101de408372d57c8
abekorsequencebio <146199840+abekorsequencebio@users.noreply.github.com>
2025-06-12 14:42:34 -0230

    update bin folder and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@ r1 = Restaurant.create!(
   name: "St. John's Fish Exchange Kitchen & Wet Bar",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "https://sjfx.ca/",
+  latitude: 47.56039315016426,
+  longitude: -52.71058174891693
 )
 
 r1.deals.create!(
@@ -16,7 +18,7 @@ r1.deals.create!(
   start_time: "16:30",
   end_time: "18:30",
   description: "Here’s one part of the weekday you’ll look forward to — Happy Hour.
-Join us from 4:30–6:30 pm at the bar!"
+  Join us from 4:30–6:30 pm at the bar!"
 )
 
 # ---- Restaurant 2 Seed Data ----
@@ -24,7 +26,9 @@ r2 = Restaurant.create!(
   name: "Bernard Stanley Gastropub",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "http://www.bernardstanleygastropub.com/",
+  latitude: 47.56720553251499,
+  longitude: -52.704883664261565
 )
 
 r2.deals.create!(
@@ -39,7 +43,9 @@ r3 = Restaurant.create!(
   name: "SHAMROCK CITY",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "http://shamrockcity.ca/",
+  latitude: 47.56445989033737,
+  longitude: -52.713260541934346
 )
 
 r3.deals.create!(
@@ -61,7 +67,9 @@ r4 = Restaurant.create!(
   name: "Sláinte",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "https://localpub.com",
+  latitude: 47.571392204546584,
+  longitude: -52.70390868940153
 )
 
 r4.deals.create!(
@@ -77,7 +85,9 @@ r5 = Restaurant.create!(
   name: "Landwash Brewery",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "http://landwashbrewery.com/",
+  latitude: 47.508352702906116,
+  longitude: -52.794260301030874
 )
 
 r5.deals.create!(
@@ -92,7 +102,9 @@ r6 = Restaurant.create!(
   name: "Magnum & Steins",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://www.magnumandsteins.ca/uploads/1/6/7/6/16762878/happy_hour_menu_2025_march-tariff.pdf"
+  website: "https://www.magnumandsteins.ca/uploads/1/6/7/6/16762878/happy_hour_menu_2025_march-tariff.pdf",
+  latitude: 47.564222341563834,
+  longitude: -52.70794951637276
 )
 
 r6.deals.create!(
@@ -108,7 +120,9 @@ r7 = Restaurant.create!(
   name: "The Cannery",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "https://www.cannerysocial.com/reservations-st-johns",
+  latitude: 47.55712153488684,
+  longitude: -52.715046391244684
 )
 
 r7.deals.create!(
@@ -123,7 +137,9 @@ r8 = Restaurant.create!(
   name: "The Gypsy Tea Room",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "http://www.gypsytearoom.ca/",
+  latitude: 47.561876098739816,
+  longitude: -52.70986712986412
 )
 
 r8.deals.create!(
@@ -139,7 +155,9 @@ r9 = Restaurant.create!(
   name: "Celtic Hearth",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "https://www.facebook.com/TheCelticHearth/",
+  latitude: 47.562088667103716,
+  longitude: -52.7099802452091
 )
 
 r9.deals.create!(
@@ -157,7 +175,9 @@ r10 = Restaurant.create!(
   name: "RABBLE",
   address: "123 Main St",
   phone: "555-1234",
-  website: "https://localpub.com"
+  website: "https://www.tbdine.com/book/restaurant/rabble-restaurant?idApp=71754&language=en-us",
+  latitude: 47.56400142103097,
+  longitude: -52.70722992193435
 )
 
 r10.deals.create!(
